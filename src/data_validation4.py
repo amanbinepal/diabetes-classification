8ddcd4e8334a698f9bfb8b9f01d565e91be305d5
Vy Phan <phanvy.ftu2@gmail.com>
2025-12-05 15:13:19 -0800

    Script 8: Check rows missing values

---
+++ src/data_validation4.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pointblank as pb
+
+# Read in the CSV file - from split_data.py script output
+train_df = pd.read_csv('objects/train_df.csv')
+
+# Data Validation - Check that all rows are complete with no missing values
+validation_3 = (
+    pb.Validate(data=train_df)
+    .rows_complete() 
+    .interrogate()
+)
+
+# Check if validation passed and save data validation results
+if validation_3.all_passed():
+    pd.DataFrame({'result': ['PASS']}).to_csv('objects/pass_data_validation3.txt', index=False)
+else:
+    pd.DataFrame({'result': ['FAILED']}).to_csv('objects/failed_data_validation3.txt', index=False)
