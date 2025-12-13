70e9d1227950689fde24b39c885d8e0cd2182b2c
iangault <142769857+iangault@users.noreply.github.com>
2025-12-12 18:54:18 -0800

    adding function documentation

--- src/functions/read_model.py
+++ src/functions/read_model.py
@@ -2,6 +2,21 @@ import os
 import pickle
 
 def read_model(out_dir):
+    """
+    This function reads in models from a designation directory
+
+    Parameters
+    ----------
+    out_dir: directory where model are stored
+
+    Returns:
+        two models needed for testing
+
+    Examples
+    --------
+    >>> lr_pipe, linear_svc_pipe = read_model(out_dir)
+    """
+
     lr_pipe_path = os.path.join(out_dir, "trained_lr_pipe.pkl")
     linear_svc_pipe_path = os.path.join(out_dir, "trained_linear_svc_pipe.pkl")
 
